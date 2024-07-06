08f9791af8544cca5d2085d85e5c94f2e3552cd1
Lars van Vianen <lars@vianen.com>
2024-07-06 15:38:06 +0200

    Create __init__.py

---
+++ src/django-ifc/fields/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Django IFC Fields Module
+========================
+
+This module provides custom Django model fields tailored to handle specific
+data types used in the IFC (Industry Foundation Classes) standard for
+building and construction data. These fields ensure compliance with IFC data
+specifications, enabling more accurate data management and exchange within
+Django applications.
+
+"""
+
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Import | Local Modules
+from .model import (
+    IfcGloballyUniqueIdField,
+    IfcLabelField,
+    IfcTextField,
+)
+
+
+# =============================================================================
+# Module Level Variables
+# =============================================================================
+
+__all__ = [
+    "IfcGloballyUniqueIdField",
+    "IfcLabelField",
+    "IfcTextField",
+]
