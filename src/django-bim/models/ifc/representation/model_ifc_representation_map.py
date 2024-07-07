ca605abd6c2fac5688d0a1b6a271c97ab67bbe3c
Lars van Vianen <lars@vianen.com>
2024-07-07 12:31:53 +0200

    Create model_ifc_representation_map.py

---
+++ src/django-bim/models/ifc/representation/model_ifc_representation_map.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides IFC ... Model Class
+===================================
+
+For more information, refer to:
+
+"""  # noqa E501
+
+
+# =============================================================================
+# Import
+# =============================================================================
+
+# Import | Standard Library
+
+# Import | Libraries
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Import | Local Modules
+from ...fields.model import (
+    IfcLabelField,
+    IfcRoleTypeField,
+    IfcTextField,
+)
+
+
+# =============================================================================
+# Classes
+# =============================================================================
