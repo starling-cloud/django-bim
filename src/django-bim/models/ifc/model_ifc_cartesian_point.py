f1d3f14578be115b8843eee7556144ceb3edfb19
Lars van Vianen <lars@vianen.com>
2024-07-07 12:31:58 +0200

    Update model_ifc_cartesian_point.py

--- src/django-bim/models/ifc/model_ifc_cartesian_point.py
+++ src/django-bim/models/ifc/model_ifc_cartesian_point.py
@@ -1,3 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides IFC ... Model Class
+===================================
+
+For more information, refer to:
+
+"""  # noqa E501
+
+
+# =============================================================================
+# Import
+# =============================================================================
+
+# Import | Standard Library
+
+# Import | Libraries
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Import | Local Modules
+from ...fields.model import (
+    IfcLabelField,
+    IfcRoleTypeField,
+    IfcTextField,
+)
+
+
+# =============================================================================
+# Classes
+# =============================================================================
+
 class IfcCartesianPoint(IfcRepresentationItem):
     """
     Model representing an IfcCartesianPoint, which defines a point in 2D or
@@ -27,3 +65,7 @@ class IfcCartesianPoint(IfcRepresentationItem):
 # =============================================================================
 # Module Variables
 # =============================================================================
+
+__all__ = [
+    "IfcActorRoleModel",
+]
