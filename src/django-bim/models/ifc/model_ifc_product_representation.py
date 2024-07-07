94916b401a8348a04e09d40eb08d27c01be82e1b
Lars van Vianen <lars@vianen.com>
2024-07-07 12:31:20 +0200

    Update model_ifc_product_representation.py

--- src/django-bim/models/ifc/model_ifc_product_representation.py
+++ src/django-bim/models/ifc/model_ifc_product_representation.py
@@ -1,3 +1,50 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides IFC ... Model Class
+===================================
+
+For more information, refer to:
+
+"""  # noqa E501
+
+
+# =============================================================================
+# Import
+# =============================================================================
+
+# Import | Standard Library
+
+# Import | Libraries
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Import | Local Modules
+from ...fields.model import (
+    IfcLabelField,
+    IfcRoleTypeField,
+    IfcTextField,
+)
+
+
+# =============================================================================
+# Classes
+# =============================================================================
+
 class IfcProductRepresentation(models.Model):
     # Simple representation for product representation details
     details = models.TextField(verbose_name=_("Representation Details"))
+
+
+# =============================================================================
+# Module Variables
+# =============================================================================
+
+__all__ = [
+    "IfcActorRoleModel",
+]
